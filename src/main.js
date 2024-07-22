92a21537eebd7932b29aa1df87f157ba77c4f3b9
miljakz <145482434+miljakz@users.noreply.github.com>
2024-07-22 08:52:49 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,20 +2,20 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import Axios from 'axios';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
-import Axios from 'axios'; // Import Axios for global HTTP requests
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAP-8IoWSYxNhvNAHq18yOe5S1B6nZKJQ4",
-  authDomain: "task-e1b5f.firebaseapp.com",
-  projectId: "task-e1b5f",
-  storageBucket: "task-e1b5f.appspot.com",
-  messagingSenderId: "877382835559",
-  appId: "1:877382835559:web:b315dbc787c26643d323c2",
-  measurementId: "G-PZGJGBVYD5"
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
