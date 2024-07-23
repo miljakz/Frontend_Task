cc8b4c3faa5e19fcab370606ba624dfa9e8ab13e
miljakz <145482434+miljakz@users.noreply.github.com>
2024-07-23 14:52:27 +0200

    Create Navbar.vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <nav>
+    <router-link to="/">Home</router-link>
+    <router-link to="/register">Register</router-link>
+    <router-link to="/login">Login</router-link>
+    <router-link to="/dashboard">Dashboard</router-link>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navbar'
+};
+</script>
+
+<style scoped>
+nav {
+  display: flex;
+  gap: 1rem;
+}
+</style>
