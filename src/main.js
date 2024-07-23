4f27cb6e95279cf76ca7e486e19ba7c45b04c972
miljakz <145482434+miljakz@users.noreply.github.com>
2024-07-23 14:12:37 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -5,10 +5,8 @@ import store from './store';
 
 // Import Firebase and its services
 import { initializeApp } from 'firebase/app';
-import { getAuth } from 'firebase/auth';
-import { getFirestore } from 'firebase/firestore';
 
-// Firebase configuration
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
   authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
@@ -20,9 +18,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const firebaseApp = initializeApp(firebaseConfig);
-const auth = getAuth(firebaseApp);
-const db = getFirestore(firebaseApp);
+initializeApp(firebaseConfig);
 
 Vue.config.productionTip = false;
 
