a8d126afcdb5f028049cb6442a7b195d433973e1
miljakz <145482434+miljakz@users.noreply.github.com>
2024-08-17 20:36:16 +0200

    Update index.vue

---
+++ src/views/index.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="index-page">
+    <header>
+      <h1>Welcome to TaskPal</h1>
+    </header>
+    <main>
+      <p>
+        Manage your tasks efficiently and boost your productivity with TaskPal. 
+        Start organizing your tasks now!
+      </p>
+      <router-link to="/register" class="button">Get Started</router-link>
+      <router-link to="/login" class="button">Login</router-link>
+    </main>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Index'
+};
+</script>
+
+<style scoped>
+.index-page {
+  text-align: center;
+  margin: 20px;
+}
+
+header {
+  margin-bottom: 20px;
+}
+
+h1 {
+  font-size: 2.5em;
+  color: #42b983;
+}
+
+p {
+  font-size: 1.2em;
+  margin-bottom: 20px;
+}
+
+.button {
+  display: inline-block;
+  margin: 10px;
+  padding: 10px 20px;
+  font-size: 1em;
+  color: white;
+  background-color: #42b983;
+  border: none;
+  border-radius: 5px;
+  text-decoration: none;
+}
+
+.button:hover {
+  background-color: #36966c;
+}
+</style>
