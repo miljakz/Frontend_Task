b049423037602b0ae15cba5cd61d23924ce86f2e
miljakz <145482434+miljakz@users.noreply.github.com>
2024-08-19 09:12:50 +0200

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="home">
     <CategoryManagerComponent />
     <TaskManagerComponent />
   </div>
@@ -10,9 +10,21 @@ import CategoryManagerComponent from '@/components/CategoryManager.vue';
 import TaskManagerComponent from '@/components/TaskManager.vue';
 
 export default {
+  name: 'HomePage',  // Added multi-word name
   components: {
     CategoryManagerComponent,
     TaskManagerComponent
   }
 };
 </script>
+
+<style scoped>
+.home {
+  padding: 20px;
+}
+
+.home h1 {
+  margin-bottom: 20px;
+  color: #42b983;
+}
+</style>
